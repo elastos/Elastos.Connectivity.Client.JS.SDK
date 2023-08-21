d6494765d25559c158659c98f39c600cd43fcaad
Benjamin Piette <benjaminpiette@elastos-fusion.org>
2023-08-21 18:07:14 +0800

    Fixed svelte UI created on init when not needed and creating an exception

--- src/services/global.modal.service.ts
+++ src/services/global.modal.service.ts
@@ -1,19 +1,20 @@
-import type { IKeyValueStorage } from "../interfaces/ikeyvaluestorage";
-import { DefaultKeyValueStorage } from "../internal/defaultkeyvaluestorage";
 import ModalContainer from "../internal/defaultui/shared/ModalContainer.svelte";
 import { getGlobalSingleton } from "../singleton";
 
 class GlobalModalService {
-    private genericModalContainer = new ModalContainer({
-        target: document.body
-    });
+	private genericModalContainer: ModalContainer;
 
-  constructor() {}
+	constructor() { }
 
-  public getModal(): ModalContainer {
-      return this.genericModalContainer;
-  }
+	public getModal(): ModalContainer {
+		if (!this.genericModalContainer) {
+			this.genericModalContainer = new ModalContainer({
+				target: document.body
+			});
+		}
+
+		return this.genericModalContainer;
+	}
 }
 
-export const globalModalService = getGlobalSingleton<GlobalModalService>("modal", ()=>new GlobalModalService());
-
+export const globalModalService = getGlobalSingleton<GlobalModalService>("modal", () => new GlobalModalService());
