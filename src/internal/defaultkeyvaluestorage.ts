16d8956dfddfaaa5861dd7c2c2046fc5a9e4dbb2
Benjamin Piette <benjaminpiette@elastos-fusion.org>
2023-09-12 21:03:10 +0800

    Changed local storage to KVS cross browser/nodejs storage

--- src/internal/defaultkeyvaluestorage.ts
+++ src/internal/defaultkeyvaluestorage.ts
@@ -1,15 +1,33 @@
+import { kvsEnvStorage } from "@kvs/env";
 import type { IKeyValueStorage } from "../interfaces/ikeyvaluestorage";
 
 /**
  * Default implementation for storing data, in case no provider is passed.
+ *
+ * NOTE: We use KVS instead of localStorage to solve potential issues with SSR, for
+ * example using nextjs that tries to generate pages for both server and client sides (both using the browser SDK though)
  */
 export class DefaultKeyValueStorage implements IKeyValueStorage {
+    private storage: any;
+
+    private async init() {
+        if (this.storage)
+            return;
+
+        this.storage = await kvsEnvStorage({
+            name: "connectivity-sdk-storage",
+            version: 1
+        });
+    }
+
     async set(key: string, value: string): Promise<void> {
-        window.localStorage.setItem(key, value);
+        await this.init();
+        await this.storage.set(key, value);
     }
 
     async get(key: string, defaultValue: string): Promise<string> {
-        let value = window.localStorage.getItem(key);
+        await this.init();
+        let value = await this.storage.get(key);
         if (value)
             return value as string;
         else
@@ -17,6 +35,7 @@ export class DefaultKeyValueStorage implements IKeyValueStorage {
     }
 
     async unset(key: string): Promise<void> {
-        window.localStorage.removeItem(key);
+        await this.init();
+        await this.storage.delete(key);
     }
 }
